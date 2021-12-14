053b980a9b4f5e7fb22c9058191d6f81d3a19308
Le Nguyen <lenguyen1612@gmail.com>
2021-12-15 00:57:42 +0700

    Applied TouchAction  part 1

--- src/main/java/tests/TestDriverFactory.java
+++ src/main/java/tests/TestDriverFactory.java
@@ -4,9 +4,12 @@ import driver.DriverFactory;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.interactions.touch.TouchActions;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static io.appium.java_client.touch.WaitOptions.waitOptions;
@@ -29,18 +32,24 @@ public class TestDriverFactory {
             //input password
             MobileElement password = androidDriver.findElementByAccessibilityId("input-password");
             password.sendKeys("passwordne");
-            password.click();
+//            password.click();
 
-            //click button login
-//            Dimension size = androidDriver.manage().window().getSize();
-//            int anchor = (int) (size.width * 0.2);
-//            int startPoint = (int) (size.height  * 0.4);
-//            int endPoint = (int) (size.height  * 0.1);
-//            TouchAction action = new TouchAction(androidDriver);
-//            action.press(point(startPoint, anchor))
-//                    .waitAction(waitOptions(ofMillis(1000)))
-//                    .moveTo(point(endPoint, anchor))
-//                    .release().perform();
+            Dimension size = androidDriver.manage().window().getSize();
+            System.out.println("width: " + size.width + " height: " + size.height);
+            int xStartPoint = (int) (size.width * 0.3);
+            int yStartPoint = (int) (size.height  * 0.9);
+            int yEndPoint = (int) (size.height  * 0.4);
+
+            //convert  to PointOption - Coorinates
+            PointOption startPoint = new PointOption().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption().withCoordinates(xStartPoint, yEndPoint);
+
+            TouchAction action = new TouchAction(androidDriver);
+            action.press(startPoint)
+                    .waitAction(new WaitOptions().withDuration(Duration.ofSeconds(5)))
+                    .moveTo(endPoint)
+                    .release().perform();
+
             MobileElement btnLogin = androidDriver.findElementByAccessibilityId("button-LOGIN");
             btnLogin.click();
         }
